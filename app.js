6a3da4a85ac04da48929c0fcf00690a59d3f83cb
yonatan gershfeld <yonatan310867@gmail.com>
2023-04-09 14:37:06 +0300

    ACTUALLY MADE A FULL GRID IT WORKSSSS

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
 let divContainer =document.querySelector(".grid-container");
 
-let num=256;
+let num=16;
 
 function createDivs(num){
     for(let i = 0; i <  num; i++){
-        const newdiv = document.createElement("div");
-        newdiv.classList.add("pixel");
-        newdiv.innerHTML= i;
-        newdiv.style.flexBasis = 0;
-        divContainer.appendChild(newdiv);
+        const rowContainer = document.createElement("div");
+        rowContainer.classList.add("row");
+        divContainer.appendChild(rowContainer);
+        for(let j = 0; j < num; j++){
+            const cell = document.createElement("div");
+            rowContainer.appendChild(cell);
+            cell.classList.add("pixel");
+        }
     }
 }
 createDivs(num);
