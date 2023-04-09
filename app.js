9cb7f0e6a0ad951b7b667b2e0a07cf30b01c89af
yonatan gershfeld <yonatan310867@gmail.com>
2023-04-09 13:26:55 +0300

    got the cells to fill the grid

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 
 let divContainer =document.querySelector(".grid-container");
 
-let num=4;
+let num=256;
 
 function createDivs(num){
-    for(let i = 0; i <  num * num; i++){
+    for(let i = 0; i <  num; i++){
         const newdiv = document.createElement("div");
         newdiv.classList.add("pixel");
         newdiv.innerHTML= i;
-        newdiv.style.flexBasis = (100 / num) + '%';
+        newdiv.style.flexBasis = 0;
         divContainer.appendChild(newdiv);
     }
 }
-
 createDivs(num);
 
